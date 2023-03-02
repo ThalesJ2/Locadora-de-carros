613732ab3a6ad2c99d59d4e97a488576d6c2ec00
Thales Jorge <thalesjorge36@gmail.com>
2023-03-02 13:48:40 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import connection from "./database/connection.js";
+import dotenv from "dotenv";
 import route from "./routes/routes.js";
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(route);
 
-app.listen(8080,()=>{
+app.listen(process.env.db_port,()=>{
     console.log("App rodando");
 });
